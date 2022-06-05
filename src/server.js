0aac9c928bf1f42c5026e88bc7fa6cfb873392ed
Tomas O Dalaigh <tom1996.18@gmail.com>
2022-06-05 19:27:50 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ if (result.error) {
 
 async function init() {
   const server = Hapi.server({
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 4000,
   });
 
   await server.register(Inert);
